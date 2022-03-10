8b492e31654be7d27e79d13cdb7908b45a31dbd4
Rahmaniii <i.raymon57@gmail.com>
2022-03-10 23:56:43 +0000

    using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Use the function puts to print the strings
+ * Description: Print some strings with puts
+ * Return: 0(success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
