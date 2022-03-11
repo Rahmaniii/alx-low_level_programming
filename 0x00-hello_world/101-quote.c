1366d02acfadf992f415f2cfaa3e674d86db85e5
Rahmaniii <i.raymon57@gmail.com>
2022-03-11 02:32:55 +0000

    printing with write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - c program printing
- * description: using putchar
+ * main - c program printing with write
+ * description: using write and standard error
  * Return: 1(success)
  */
 
 int main(void)
 {
-	int putchar("and that piece of art is useful\" - Dora Korpar,2015-10-19")
+	write(2, "and that piece of art is useful\" - Dora Korpar,2015-10-19\n", 58);
 	return (1);
 }
