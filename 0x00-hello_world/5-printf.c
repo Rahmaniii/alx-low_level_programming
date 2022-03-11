978cef4be81585ed31fca893743ce0b60c84d211
Rahmaniii <i.raymon57@gmail.com>
2022-03-11 00:36:59 +0000

    printing using printf]

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - printf should be used
+ * description - Using printf to print strings
+ * Return: 0(success)
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
+
