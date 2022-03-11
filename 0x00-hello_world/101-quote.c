ea69230c06e5ed77985d9a480a353e0e1c4a841e
Rahmaniii <i.raymon57@gmail.com>
2022-03-11 02:02:41 +0000

    wa

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - c program printing
+ * description: using putchar
+ * Return: 1(success)
+ */
+
+int main(void)
+{
+	int putchar("and that piece of art is useful\" - Dora Korpar,2015-10-19")
+	return (1);
+}
