ecb6c79a94213320bb62d3ca7c91ce554612fcbe
Rahmaniii <i.raymon57@gmail.com>
2022-03-14 00:38:08 +0000

    corrections

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,37 +8,23 @@
 
 int main(void)
 {
-	int dig11;
-	int dig12;
-	int dig21;
-	int dig22;
+	int num1, num2;
 
-	for (dig11 = '0'; dig11 <= '9'; dig11++)
+	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for (dig12 = '0'; dig12 <= '9'; dig12++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			for (dig21 = dig11; dig21 <= '9'; dig21++)
-			{
-				for (dig22 = dig12 + 1; dig22 <= '9'; dig22++)
-				{
-					putchar(dig11);
-					putchar(dig12);
-					putchar(' ');
-					putchar(dig21);
-					putchar(dig22);
-
-					if (!((dig11 == '9' && dig12 == '8') &&
-						(dig21 == '9' && dig22 == '9')))
-						{
-							putchar(',');
-							putchar(' ');
-						}
-				}
-				dig22 = '0';
-			}
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+			if (num1 == 98 && num2 == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
