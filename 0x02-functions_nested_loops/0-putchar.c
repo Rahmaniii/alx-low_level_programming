cdc7ce9c45b8d6cd8e6328f8e70928b86d951d5f
Rahmaniii <i.raymon57@gmail.com>
2022-03-16 02:08:09 +0000

    adding next line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-	_putchar(' ');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -15,6 +15,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
