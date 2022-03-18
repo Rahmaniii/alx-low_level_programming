cfad02a8c5629fcd0f5de5f7f7460256b02225a0
Rahmaniii <i.raymon57@gmail.com>
2022-03-18 02:24:07 +0000

    nochecker

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,15 +8,17 @@
 void more_numbers(void)
 {
 	int i;
-	int j;
+	char j;
 
 	for (i = 0; i <= 10; i++)
 	{
 		for (j = 0; j <= 4; j++)
 		{
-			_putchar(j);
+
+		if (j / 10 > 0)
+			_putchar((j / 10) + '0');
+		_putchar((i % 10) + '0');
 		}
-		_putchar(i);
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
