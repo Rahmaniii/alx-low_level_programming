d29ef6475646484beda15dc6a892130088ed6377
Rahmaniii <i.raymon57@gmail.com>
2022-03-24 23:38:41 +0000

    string lenght

---
+++ 0x05-pointers_arrays_strings/strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - returns lenght of a string
+ * @s: lenght
+ * Return: 0(always)
+ */
+
+int _strlen(char *s)
+{
+	int lnght = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
