ea4030e58ee99f90bc9aebe31cc3986361c334a7
Rahmaniii <i.raymon57@gmail.com>
2022-05-24 00:16:59 +0000

    freeing d

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -5,15 +5,17 @@
  * free_dog - frees element of dog
  * @d: pointer to dog
  *
- * Return: 0
+ * Return: void
  */
 
 void free_dog(dog_t *d)
 {
+	if (d == NULL)
+		return;
 	if (d)
-	{
 		free(d);
+	if ((*d).name)
 		free((*d).name);
+	if ((*d).owner)
 		free((*d).owner);
-	}
 }
